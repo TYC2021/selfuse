427875103ab15f4554cc6f6fd17f7dae9484d07f
TYC2021 <93442122+TYC2021@users.noreply.github.com>
2022-11-14 11:58:47 +0800

    Update PDFExpert.js

--- files/JS/PDFExpert.js
+++ files/JS/PDFExpert.js
@@ -2,7 +2,7 @@
 PDF Expert Unlock
 QuantumultX:
 [rewrite_local]
-Documents-PdfExpert = type=http-response,pattern=^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/TYC2021/selfuse/main/files/JS/PDFExpert.js,script-update-interval=-1
+Documents-PdfExpert url script-response-body ^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/TYC2021/selfuse/main/files/JS/PDFExpert.js,script-update-interval=-1
 hostname = %APPEND% license.pdfexpert.com
 [MITM]
 hostname = license.pdfexpert.com
