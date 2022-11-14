5d20b5ca7ec6bbc232c13f3b894b3ef84a7ed2f2
TYC2021 <93442122+TYC2021@users.noreply.github.com>
2022-11-14 17:25:27 +0800

    Update PicsArt.js

--- files/JS/PicsArt.js
+++ files/JS/PicsArt.js
@@ -9,7 +9,7 @@ hostname = api.picsart.c*, api.meiease.c*
 ***************************
 Surge4 or Loon:
 [Script]
-http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/PicsArt.js
+http-response https:\/\/api\.(picsart|meiease)\.c(n|om)\/users\/show\/me\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/TYC2021/selfuse/main/files/JS/PicsArt.js
 [MITM]
 hostname = api.picsart.c*, api.meiease.c*
 **************************/
